bec48be04277b079c1663b8a657aecf9e18f710b
ismail BOZKURT <ismailbozkurt@ymail.com>
2021-04-23 16:42:27 +0300

    Çalışma-4 1-Entites katmanı içerisine paketlerden Entity Framework paketi eklendi. 2-Classlar içerisinde ID li propertylere primary key olarak kullanılacağı için [KEY] DAtaAnnotation sınıfına ait attribute kullanıldı. 3-Classlar içerisinde isim uzunluğu kısıtlaması yapılacak propertylere [StringLength()] DataAnnotation sınıfına ait attribute kullanıldı. 4-Veritabanına tabloları yansıtmak için DataAccess katmanı içerisinde Context isminde class oluşturuldu. 5-Context sınıfı içerisinde tanımlanacak propertyler sql serverde birer tablo ismine karşılık bulacaktır. 6-DataAccess katmanı içerisinde paketlerden Entity Framework paketi eklendi. 7-Context sınıfına DBContext sınıfına ait özellikler inherit edildi. 8-Başka katmanının sınıflarını,metodlarını,propertylerini kullanılmak isteniyorsa katmanı kullanılacak yere reference olarak eklenmesi gerekir. 9-Bunun içinde DataAccess katmanında Entities katmanına ait sınıfları kullanabilmek için reference olarak DataAccess katmanına eklendi. 10-Context sınıfı içerisinde entities classları-tablolar arası karşılık gelecek DBSet<Entities> tipinde propertyler yazıldı. 11-Sunum katmanı içerisinde mevcut bulunan Web.Config dosyası proje ile ilgili yapılandırma ayarlarının yapıldığı kısım.Projenin veritabanı bağlantı adresi bu dosya içerisinde tanımlanıyor.Projede 404 sayfaları ile ilgili ayarlar burda tanımlanıyor.Projenin oturum ayarları,yetkilendirme ayaları bu dosya üzerinde gerçekleştiriliyor. 12-Veritabanına bağlantı ayarı gerçekleştireceğimiz için Sunum katmanında  Web.Config dosyası içerisinde </System.Web> tagından sonra <connectionStrings> </connectionString> tagı içerisin bağlantı stringi oluşturuldu.

--- Entities/Concrete/Writer.cs
+++ Entities/Concrete/Writer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,22 @@ namespace Entities.Concrete
 {
     public class Writer
     {
+        [Key]
         public int WriterID { get; set; }
+
+        [StringLength(50)]
         public string WriterName { get; set; }
+
+        [StringLength(50)]
         public string WriterSurname { get; set; }
+
+        [StringLength(100)]
         public string WriterImage { get; set; }
+
+        [StringLength(50)]
         public string WriterMail { get; set; }
+
+        [StringLength(20)]
         public string WriterPassword { get; set; }
 
         //one-N ilişkinin one tarafı
